6738736ccc7366ac4f062e586fbc5fb86eef5362
Terormumuli <terahmumuli@gmail.com>
2022-06-14 09:22:13 +0300

    Event Listening Lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    input.addEventListener('click', function(event) {
+        return "clicked"
+      });
+    }
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
